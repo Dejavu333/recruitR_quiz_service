9ec0476d3d71923f1c9bf2cfd09cd3f5fe448443
Botond Vasvári <botyware@gmail.com>
2023-07-28 18:52:27 +0200

    add constants

---
+++ recruitR_quiz_service/Controller/Msg.cs
@@ -0,0 +1,10 @@
+﻿namespace recruitR_quiz_service;
+
+public static class Msg
+{
+    public const string INSERTED = "inserted successfully";
+    public const string DELETED = "deleted successfully";
+    public const string UPDATED = "updated successfully";
+    public const string NOT_FOUND = "item not found";
+    public const string TIMEOUT = "connection timeout";
+}
