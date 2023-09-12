93cbd33d73c49fe4579d1afabae6c0a0719d7f5d
botond.vasvari <botyware@gmail.com>
2023-09-12 16:53:27 +0200

    adds owneremail property to QuizDTO, adds filters to query quizzes

--- recruitR_quiz_service/Src/Domain/QuizDTO.cs
+++ recruitR_quiz_service/Src/Domain/QuizDTO.cs
@@ -10,25 +10,30 @@ public class QuizDTO
     //---------------------------------------------
     // fields, properties
     //---------------------------------------------
-    [BsonId]
+    [BsonId] 
     public string? id { get; set; }
-    
-    [Required] [StringLength(150, MinimumLength = 3)]
+
+    [Required]
+    [StringLength(150, MinimumLength = 3)]
     public string? title { get; set; }
+
+    [EmailAddress] 
+    public string? ownerEmail { get; set; }
 
     [Range(60, double.MaxValue)]
     public int? overallTimeLimit { get; set; } //todo alg so can't be exploited on the client's side
-    
+
     [HasMoreElementsThan(0)] 
     public List<QuizQuestionDTO>? quizQuestions { get; set; }
 
     //---------------------------------------------
     // constructors
     //---------------------------------------------
-    public QuizDTO(string? id, string? title, int? overallTimeLimit, List<QuizQuestionDTO>? quizQuestions)
+    public QuizDTO(string? id, string? title, string? ownerEmail, int? overallTimeLimit, List<QuizQuestionDTO>? quizQuestions)
     {
         this.id ??= ObjectId.GenerateNewId().ToString();
         this.title = title;
+        this.ownerEmail = ownerEmail;
         this.overallTimeLimit = overallTimeLimit;
         this.quizQuestions = quizQuestions;
     }
--- recruitR_quiz_service/Src/Usecases/RetrieveQuizzesForUserUI/RetrieveQuizzesForUserUI_REST_V1.cs
+++ recruitR_quiz_service/Src/Usecases/RetrieveQuizzesForUserUI/RetrieveQuizzesForUserUI_REST_V1.cs
@@ -1,4 +1,5 @@
-﻿using recruitR_quiz_service.Service;
+﻿using System.Linq.Expressions;
+using recruitR_quiz_service.Service;
 using Microsoft.AspNetCore.Mvc;
 using recruitR_quiz_service.Repository;
 
@@ -20,6 +21,7 @@ public class RetrieveQuizzesForUserUI_REST_V1 : ControllerBase
 
     public record Request
     {
+        public string? owneremail { get; set; }
         public string? id { get; set; }
         public string? title { get; set; }
     }
@@ -43,30 +45,60 @@ public class RetrieveQuizzesForUserUI_REST_V1 : ControllerBase
     {
         _logger?.Debug("retrieving quizzes for user UI");
 
-        if (req.title is null && req.id is null)
+        if (req.title is null && req.id is null && req.owneremail is null)
         {
+            // Case 1: All properties are null, retrieve all quizzes
             var quizzes = _quizRepository.ReadQuizzes();
             if (quizzes.Count == 0) return NotFound();
             else return Ok(quizzes);
         }
-
-        if (req.id is null)
-        {
-            var quiz = _quizRepository.ReadQuiz(quizInDb => quizInDb.title == req.title);
-            if (quiz is null) return NotFound();
-            else return Ok(new List<QuizDTO> { quiz });
-        }
-        else if (req.title is null)
-        {
-            var quiz = _quizRepository.ReadQuiz(quizInDb => quizInDb.id == req.id);
-            if (quiz is null) return NotFound();
-            else return Ok(new List<QuizDTO> { quiz });
-        }
         else
         {
-            var quiz = _quizRepository.ReadQuiz(quizInDb => quizInDb.title == req.title && quizInDb.id == req.id);
+            // Initialize filters to build the query based on non-null conditions
+            var filters = new List<Expression<Func<QuizDTO, bool>>>();
+
+            if (!string.IsNullOrWhiteSpace(req.title))
+            {
+                // Case 2: Title is not null, search by title
+                filters.Add(quizInDb => quizInDb.title == req.title);
+            }
+
+            if (req.id != null)
+            {
+                // Case 3: ID is not null, search by ID
+                filters.Add(quizInDb => quizInDb.id == req.id);
+            }
+
+            if (!string.IsNullOrWhiteSpace(req.owneremail))
+            {
+                // Case 4: Email is not null, search by owneremail (assuming owneremail property exists in Quiz)
+                filters.Add(quizInDb => quizInDb.ownerEmail == req.owneremail);
+            }
+
+            // Combine the filters using AND logic
+            var combinedFilter = filters.Aggregate(
+                (filter1, filter2) => filter1.And(filter2)
+            );
+
+            var quiz = _quizRepository.ReadQuiz(combinedFilter);
+
             if (quiz is null) return NotFound();
             else return Ok(new List<QuizDTO> { quiz });
         }
+    }
+}
+
+public static class ExpressionExtensions
+{
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
+    {
+        var invokedExpr = Expression.Invoke(right, left.Parameters);
+        return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(left.Body, invokedExpr), left.Parameters);
+    }
+
+    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
+    {
+        var invokedExpr = Expression.Invoke(right, left.Parameters);
+        return Expression.Lambda<Func<T, bool>>(Expression.OrElse(left.Body, invokedExpr), left.Parameters);
     }
 }
